bc9458a013347b094775f297e5a962174d68f867
Nikolas Louret <nikoul.ret@gmail.com>
2024-06-27 17:44:01 -0300

    feat: criacao e configuração do projeto node #1

---
+++ code/src/express.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { json } from 'body-parser';
+import { get } from 'config';
+
+export default () => {
+  const app = express();
+
+  app.set('port', process.env.PORT || get('server.port'));
+  app.use(json());
+
+  return app;
+};
