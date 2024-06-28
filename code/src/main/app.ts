3a84c4240d4bb159464e8a9a93efadaa4ae81a8e
Nikolas Louret <nikoul.ret@gmail.com>
2024-06-27 22:42:39 -0300

    refactor: fix keeping Jest from exiting

--- code/src/main/app.ts
+++ code/src/main/app.ts
@@ -5,14 +5,6 @@ import router from './routes/router';
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/api', router);
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 export default app;
